a6fb4ccdfb76182a96a4090c2627f9b1ce837e3d
Shelby S <3541579+sillyfatcat@users.noreply.github.com>
2020-06-03 20:00:47 -0700

    fixed classifier on python

--- setup.py
+++ setup.py
@@ -27,10 +27,10 @@ setup(
     ],
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Intended Audience :: Developers',
-        'Topic :: Games/Entertainment',
+         'Topic :: Internet/Name Service (DNS)',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
