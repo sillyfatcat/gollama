43c82a8588358283f66ad408d671c6c9e863d2c0
Shelby S <3541579+sillyfatcat@users.noreply.github.com>
2020-06-08 19:05:10 -0700

    added feature to help more easily create new golinks

--- gollama/frontend/static/javascript/main.js
+++ gollama/frontend/static/javascript/main.js
@@ -1,7 +1,20 @@
+function get_hash_params() {
+    let hash_string = window.location.hash.substr(1)
+    let components = hash_string.split('&')
+    let keys = {}
+    for (const component in components) {
+        let kv_pair = components[component].split('=')
+        if (kv_pair[0].length) {
+            keys[kv_pair[0]] = kv_pair[1]
+        }
+    }
+    return keys
+}
+
+
 $(document).ready(function () {
     $('#search').on('keyup', function (e) {
         if (e.which == 13) {
-            console.log('enter key pressed')
             $('[name="shorthand_row"]').hide('slow', () => {
                 // TODO not sure why this callback is not triggering
                 $(`[name="shorthand_row"]:contains("${$(this).val()}")`).show('slow')
@@ -9,4 +22,11 @@ $(document).ready(function () {
             $(`[name="shorthand_row"]:contains("${$(this).val()}")`).show('slow')
         }
     })
+
+    // This is the code to initialise pulling fragment components
+    let hash_params = get_hash_params()
+    if (Object.keys(hash_params).length != 0) {
+        $('#shorthand').val(hash_params['new'])
+        $('#url').focus()
+    }
 });
