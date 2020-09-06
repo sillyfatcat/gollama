e50499b0831599b655cd9315ba935c0227ed1f0e
Shelby S <3541579+sillyfatcat@users.noreply.github.com>
2020-09-05 23:57:38 -0700

    bumped up version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(long_desc_fd) as f:
 
 setup(
     name='gollama',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     url='https://github.com/sillyfatcat/gollama',
     license='MIT',
